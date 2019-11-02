278cc81cb9c1f185b536e0fce95f51c23edf9f19
DimEnsae <dimitri.meunier@ensae-paristech.fr>
2019-11-02 20:14:23 +0100

    second cleaning

--- code.R
+++ code.R
@@ -1,17 +1,37 @@
-load("Xtest.RData")
+library(MASS)
+
+#Question 1 -----------------------------------------------------
+
+load("Xtest.RData") #chargement des donnees
 load("Ytest.RData")
 load("Xtrain.RData")
 load("Ytrain.RData")
-setwd("/Users/dimitri/Library/Mobile Documents/com~apple~CloudDocs/M1ENSAE/Semestre1/Statistique1/QDA_Classifier")
-getwd()
-list.files()
+obs = length(Ytrain) + length(Ytest)
+print(c("Number of observations: ", obs))
+print(c("Number of observations in train set: ",length(Ytrain))) #numbers of images * 40 000
+print(c("Number of observations in test set: ",length(Ytest)))
+print(c("Dimension of each observation: ",length(Xtrain[1,])))
+par(mfrow=c(2,2), mai=c(0.8,0.1,0.1,0.1)) #display images on a grid
+for (i in c(5,22,45,47)) {
+  mat = matrix(rev(Xtest[i,]), nrow=200, byrow=TRUE)#resize matrix, #dim(mat)=200*200
+  string = "Image of a"
+  if (Ytest[i]==1) string = paste(string,"cat, labelled",toString(1))
+  else string = paste(string,"dog, labelled ",toString(0))
+  
+  image(mat,col = grey(seq(0, 1, length = 256)), xaxt= "n", yaxt= "n", xlab=string  ) #Display image
+}
 
-compute_PCA <-function() {
-  #load("/Xtest.RData") #chargement des donnees
-  #load("/Xtrain.RData")
+print(c("Percentage of positive targets (cats) in train set",sum(Ytrain==1)/length(Ytrain)))
+print(c("Percentage of positive targets (cats) in test set",sum(Ytest==1)/length(Ytest)))
+
+
+#Question 2 -----------------------------------------------------
+
+
+compute_PCA <- function(Xtrain,Xtest) {
   X = rbind(Xtrain,Xtest) #concatenation
   X = scale(X, center = TRUE, scale = FALSE)  #centrage 
-  PCA = svd(X,nu=0,nv=15) #nv=15 on ne conserve que les 15 PCA
+  PCA = svd(X,nu=0,nv=15) #nv=15 on ne conserve que 15 composantes
   C = X %*% PCA$v
   Ctrain = C[1:315,]
   Ctest = C[316:dim(C)[1],]
@@ -21,86 +41,153 @@ compute_PCA <-function() {
   return(sum(PCA$d[1:15]**2)/sum(PCA$d**2)) #variance expliquee
 }
 
-compute_PCA()
+compute_PCA(Xtrain,Xtest)
+
+
+#Question 9 -----------------------------------------------------
+
 load("Ctrain.RData")
 load("Ctest.RData")
 
 computeML <- function(C, Y){
   n = length(Y)
   N1 = sum(Y==1)
-  C1 = C[Y==1,]
   C0 = C[Y==0,]
+  C1 = C[Y==1,]
   
-  pi_hat = N1/n
-  mu_hat1 = colMeans(C1)
+  p_hat = N1/n
   mu_hat0 = colMeans(C0)
-  sigma_hat1 = t(sweep(C1,2,mu_hat1))%*%sweep(C1,2,mu_hat1)/N1
-  sigma_hat0 = t(sweep(C0,2,mu_hat0))%*%sweep(C0,2,mu_hat0)/(n-N1)
+  mu_hat1 = colMeans(C1)
+  C0_centered = sweep(C0,2,mu_hat0) #susbtract mu_hat0 to C0 
+  C1_centered = sweep(C1,2,mu_hat1)
+  sigma_hat0 = t(C0_centered)%*%C0_centered/(n-N1)
+  sigma_hat1 = t(C1_centered)%*%C1_centered/N1
   
+  out = list(p_hat,mu_hat0,mu_hat1,sigma_hat0,sigma_hat1)
   
-  out = list(pi_hat,mu_hat0,mu_hat1,sigma_hat0,sigma_hat1)
   return(out)
 }
 
-computeLogRatio <- function(c,pi,mu0,mu1,Sigma0,Sigma1) {
-  logratio = 0.5*(-log(det(Sigma1)) + log(det(Sigma0)) - t(matrix(c)-mu1)%*%Sigma1%*%(matrix(c)-mu1) + t(matrix(c)-mu0)%*%Sigma0%*%(matrix(c)-mu0)) + log(pi) - log(1-pi)
+ML = computeML(Ctrain,Ytrain) 
 
-  return(logratio)
+#QDA from MASS package
+qda.model = qda(Ctrain,Ytrain)
+
+ML = computeML(Ctrain,Ytrain) 
+#Comparaisons
+cat("Estimation of p : ", ML[[1]])
+cat("QDA estimation of p : ", qda.model$prior[2])
+cat("Estimation of log(det(sigma0)) : ", log(det(ML[[4]])))
+cat("QDA estimation of log(det(sigma0)) : ", qda.model$ldet[1])
+cat("Estimation of log(det(sigma1)) : ", log(det(ML[[5]])))
+cat("QDA estimation of log(det(sigma1)) : ", qda.model$ldet[2])
+cat("Estimation of mu0[1:4] : ",  ML[[2]][1:4])
+cat("QDA estimation of mu0[1:4] : ",  qda.model$means[1,1:4])
+cat("Estimation of mu1[1:4] : ",  ML[[3]][1:4])
+cat("QDA estimation of mu1[1:4] : ",  qda.model$means[2,1:4])
+
+
+#Question 14 -----------------------------------------------------
+
+
+computeLogRatio <- function(cvect,p,mu0,mu1,Sigma0,Sigma1) {
+  
+  logratio = (0.5*(-log(det(Sigma1)) + log(det(Sigma0)) 
+                   - t(cvect-mu1)%*%ginv(Sigma1)%*%(cvect-mu1)
+                   + t(cvect-mu0)%*%ginv(Sigma0)%*%(cvect-mu0)) 
+              + log(p) - log(1-p))
+  
+  return(logratio) 
 }
 
-
-computePred <- function(Ctrain,Ctest,Ytrain,Ytest) {
-  stat = computeML(Ctrain,Ytrain)
-  score = 0
-  for (i in 1:length(Ytest)) {
-    logratio = computeLogRatio(Ctest[i,],stat[[1]],stat[[2]],stat[[3]],stat[[4]],stat[[5]])
-    if (logratio>0) {result =1}
-    else {result = 0}
-    #print(c("Pred ",i, "is : ", result, "True result is : ", Ytest[i]))
-    if (result==Ytest[i]) {score = score + 1}
-    
+computePred <- function(C,p,mu0,mu1,Sigma0,Sigma1) {
+  
+  toapply <- function(cvect,p,mu0,mu1,Sigma0,Sigma1) {
+    return(as.integer((computeLogRatio(cvect,p,mu0,mu1,Sigma0,Sigma1)>0)))
   }
   
-  return(score/length(Ytest))
+  pred = apply(C, MARGIN = 1, FUN = toapply, p = p, mu0 = mu0, 
+               mu1 = mu1, Sigma0 = Sigma0, Sigma1 = Sigma1)
+  
+  return(pred)
 }
 
+
+#Question 15 -----------------------------------------------------
+
+stats = computeML(Ctrain,Ytrain)
+prediction = computePred(Ctest,stats[[1]],stats[[2]],stats[[3]],stats[[4]],stats[[5]])
+
+prediction
+
+#Erreur de prediction :
+sum(prediction==Ytest)/length(Ytest)
+
+computeQDA <- function(Ctrain, Ctest, Ytrain, Ytest){
+  qda.model = qda(Ctrain,Ytrain)
+  pred = predict(qda.model, Ctest)
+  print(pred$class)
+  return(sum(pred$class==Ytest)/length(Ytest))
+}
+
+computeQDA(Ctrain, Ctest, Ytrain, Ytest)
+
+
+#Bonus -----------------------------------------------------
 
 
 computeMLlin <- function(C, Y){
+  #Calcul les statistiques associees a la LDA
   n = length(Y)
   N1 = sum(Y==1)
-  C1 = C[Y==1,]
   C0 = C[Y==0,]
-  
-  pi_hat = N1/n
-  mu_hat1 = colMeans(C1)
+  C1 = C[Y==1,]
+  p_hat = N1/n
   mu_hat0 = colMeans(C0)
-  Sigma = (t(sweep(C1,2,mu_hat1))%*%sweep(C1,2,mu_hat1) + t(sweep(C0,2,mu_hat0))%*%sweep(C0,2,mu_hat0))/(n-2)
+  mu_hat1 = colMeans(C1)
+  C0_centered = sweep(C0,2,mu_hat0)
+  C1_centered = sweep(C1,2,mu_hat1)
+  Sigma = (t(C0_centered)%*%C0_centered + t(C1_centered)%*%C1_centered)/(n-2)
   
-  out = list(pi_hat,mu_hat0,mu_hat1,Sigma)
+  out = list(p_hat,mu_hat0,mu_hat1,Sigma)
   return(out)
 }
-  
-  computeLogRatiolin <- function(c,pi,mu0,mu1, Sigma) {
-    logratio = log(pi/(1-pi)) - 0.5*t(mu1 + mu0)%*%Sigma%*%(mu1 - mu0) + t(matrix(c))%*%Sigma%*%(mu1-mu0)
-    return(logratio)
-  }
 
-
-computePredlin <- function(Ctrain,Ctest,Ytrain,Ytest) {
-  stat = computeMLlin(Ctrain,Ytrain)
-  score = 0
-  for (i in 1:length(Ytest)) {
-    logratio = computeLogRatiolin(Ctest[i,],stat[[1]],stat[[2]],stat[[3]],stat[[4]])
-    if (logratio>0) {result =1}
-    else {result = 0}
-    #print(c("Pred ",i, "is : ", result, "True result is : ", Ytest[i]))
-    if (result==Ytest[i]) {score = score + 1}
-    
-  }
+computeLogRatiolin <- function(c,p,mu0,mu1, Sigma) {
+  #Calcul les log ratio associees a la LDA 
   
-  return(score/length(Ytest))
+  logratio = (log(p) - log(1-p) - 0.5*t(mu1 + mu0)%*%ginv(Sigma)%*%(mu1 - mu0) 
+              + t(c)%*%ginv(Sigma)%*% (mu1-mu0))
+  
+  return(logratio)
 }
 
-computePred(Ctrain,Ctest,Ytrain,Ytest)
-computePredlin(Ctrain,Ctest,Ytrain,Ytest)
+computePredlin <- function(C,p,mu0,mu1,Sigma) {
+  
+  nbobs = dim(C)[1]
+  pred = rep(0,nbobs)
+  for (i in 1:nbobs) {
+    
+    logratio = computeLogRatiolin(C[i,],p,mu0,mu1,Sigma) 
+    predi = logratio>0
+    pred[i] = predi
+  }
+  
+  return(pred) 
+}
+
+computeLDA <- function(Ctrain, Ctest, Ytrain, Ytest){
+  lda.model = lda(Ctrain,Ytrain)
+  pred = predict(lda.model, Ctest)
+  print(pred$class)
+  return(sum(pred$class==Ytest)/length(Ytest))
+}
+
+
+statsbis = computeMLlin(Ctrain,Ytrain)
+prediction = computePredlin(Ctest,statsbis[[1]],statsbis[[2]],statsbis[[3]],statsbis[[4]])
+prediction
+sum(prediction==Ytest)/length(Ytest)
+computeLDA(Ctrain, Ctest, Ytrain, Ytest)
+
+
